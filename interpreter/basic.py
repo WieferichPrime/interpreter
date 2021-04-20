1b5858e9243c49db0562c19029c97e14015e07b7
SupaProgramer <dania.swirdov@gmail.com>
2021-04-20 19:50:54 +0300

    commit

--- interpreter/basic.py
+++ interpreter/basic.py
@@ -1,10 +1,17 @@
 from Lexer import lex
-from Parser import CheckSyntax,Parser
+from Parser import CheckSyntax, Parser
 import sys
+str = input('>>> ')
+while str != 'exit':
+    try:
+        tokens = lex(str)
+        parser = CheckSyntax(tokens)
+        lang = parser.lang()
+        print(lang)
+        for expr in parser.exprs_token:
+            print(expr)
+        str = input('>>> ')
+    except:
+        pass
+sys.exit(0)
 
-
-str = 'if(a == 1){a = a+1;}a = 123 * 2 + 2;'
-tokens = lex(str)
-parser = CheckSyntax(tokens)
-lang = parser.lang()
-print(lang)
